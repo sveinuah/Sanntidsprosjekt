23f4bbc2525ba046e9dbee90442cca3161fd00db
Sondrenm <sondrehadet@gmail.com>
2017-02-08 13:18:36 +0100

    Prøver å fikse ting

---
+++ heis/src/master/master.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	//Init
+
+	for {
+		/*
+			- Hvem er på nettverket?
+			- Lag lister over heiser og mastere
+			- Sjekk om jeg er sjef? Hvis ikke, hopp fram til **
+			- Be om status fra alle heiser
+			**
+
+
+
+		*/
+	}
+
+}
