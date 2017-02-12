f8372f6bd9e7049306fa74ac3c22743660f4f488
sondre <sondrehadet@gmail.com>
2017-02-12 12:26:18 +0100

    Not sure if changed anything

--- heis/src/buttoninterface/buttoninterface.go
+++ heis/src/buttoninterface/buttoninterface.go
@@ -5,44 +5,47 @@ import (
 	. "../typedef"
 )
 
-var extLights [][] bool
-var intLights [] bool
+var lights [][]bool
 
-//or: 
-
-//Should this module be able to receive buttonList type arguments?
-func buttonInterface(downChan chan OrderType, upChan chan OrderType, numFloors int) {
+func buttonInterface(nameforordersthatgotoBIChan chan OrderType, buttonPressesChan chan OrderType, numFloors int) {
 	buttonInterfaceInit(numFloors)
 	for {
-		//Get new button presses and send order up
-		for floor := 0; floor < numFloors; i++ {
-			for dir := 0; dir < 3; j++ {
+		//Get new button presses and send order up/to drive
+		for floor := 0; floor < numFloors; floor++ {
+			for dir := 0; dir < 3; dir++ {
 				if elevGetButtonSignal(floor, dir) == true && lightList[floor][dir] == false { //Make variable lightList or read hardware each time?
 					var order OrderType
 					order.Floor = floor
 					order.Dir = dir
 					order.Arg = true
-					upChan <- order
+					buttonPressesChan <- order
 					if order.Dir == DIR_NODIR {
 						elevButtonLight(order.Floor, order.Dir, order.Arg)
+						allocatedOrdersChan <- order
 					}
 				}
 			}
 		}
-		//Copy extLights from master if new in channel
+		//Copy extLights from master if new in channel, set/clear lights that are wrong
 		select {
-			case TempExtLights := <- extLightsChan:
-				if()
-
-			default:
-		}				
+		case extLights := <-extLightsChan:
+			for floor := 0; floor < numFloors; floor++ {
+				for dir := 0; dir < 2; dir++ {
+					if lights[floor][dir] != extLights[floor][dir] {
+						elevButtonLight(floor, dir, extLights[floor][dir])
+						lights[floor][dir] = extLights[floor][dir]
+					}
+				}
+			}
+		default:
+		}
 		//Get new orders and set/clear lights
 		ordersInChannel := true
 		for ordersInChannel {
 			select {
 			case order := <-downChan:
 				elevButtonLight(order.Floor, order.Dir, order.Arg)
-				buttonList[Order.Floor][order.Dir] = order.Arg
+				lights[Order.Floor][order.Dir] = order.Arg
 			default:
 				ordersInChannel = false
 			}
@@ -50,10 +53,12 @@ func buttonInterface(downChan chan OrderType, upChan chan OrderType, numFloors i
 	}
 }
 
-func buttonInterfaceInit(numFloors int) {
-	for floor := 0; floor < numFloors; i++ {
-		for dir := 0; dir < 3; j++ {
+func buttonInterfaceInit() int {
+	//wait for drive to run elevInit, return numFloors
+	for floor := 0; floor < numFloors; floor++ {
+		for dir := 0; dir < 3; dir++ {
 			buttonList[floor][dir] = false
 		}
 	}
+	return numFloors
 }
