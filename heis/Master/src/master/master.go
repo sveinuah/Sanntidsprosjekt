d2dd2a783ef45620e9642bccceffa3129118ec04
Sveinung Haugane <sveihaug@gmail.com>
2017-02-12 18:19:27 +0100

    Gjort ferdig initialisering i master

--- heis/Master/src/master/master.go
+++ heis/Master/src/master/master.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"../typedef"
+	"log"
+	"time"
 )
 
 /* Har vi lyst til å definere dette i networkInterface?
@@ -27,62 +29,97 @@ type elevatorReport struct {
 
 var active bool
 var unitID int
-var unitList [] UnitType
-var masterOrderList [] Order
+var unitList[] UnitType
+
+type stack []Order
+
+func (s stack) Push(o Order) stack {
+	return append(s,o)
+}
+
+func (s stack) Pop() (stack, Order) {
+	length := len(s)
+	return s[:l-1], s[l-1]
+
+	// add error if empty stack
+}
 
 func checkIfActive() {
-	if unitList[0].Port == unitID {
-		active = true
-	} else {
-		active = false
+	active = true
+	for _, unit := range(unitList) {
+		if unit.Type == TYPE_MASTER {
+			if unitID > unit.Port {
+				active = false
+			}
+		}
 	}
 }
 
-func init() {
-	// Find other units
-	// If other masters:
-		// update from other masters
-		// ask for free port
-	// else
-		// unitID = MIN_MASTER_PORT
-	// check if active
-	// initialize variables
+func init(unitStatusChan chan UnitType, masterSync chan []Order) {
+	// broadcast "I'm here" NYI
+
+	timeOut := make(chan bool, 1)
+	go func {
+		time.Sleep(time.Second * 3)
+		timeOut <- true
+	}
+
+	done := false
+	for done != true {
+		select {
+		case unit := <- unitStatusChan:
+			unitHandler(unit)
+		case orderList := <- masterSync:
+			copy(masterOrderList, orderList)
+		case done <- timeOut
+		}
+	}
+	unitID = getUnitID()  //asks network interface for an I
 	checkIfActive()
 }
 
+func unitHandler(unit UnitType) {
+	newUnit := true
+		for i = range(unitList){
+			if i.Port == unit.Port {
+				newUnit = false
+				break
+			}
+		}
+		if(newUnit) {
+			unitList.append(unit)
+		}
+}
+
 func main() {
-	//Init
 	orderChan = make(chan OrderPackage)
 	unitStatusChan = make(chan UnitType)
 	reportChan = make(chan elevatorReport)
 	masterSync = make(chan [] Order)
 
+	init()
+
 	for {
-		
 		switch active {
 		case true:
 			select {
 			case unit := <- unitStatusChan:
-				newUnit := true
-				for i = range(unitList){
-					if i.Port == unit.Port {
-						newUnit = false
-						break
-					}
-				}
-				if(newUnit) {
-					unitList.append(unit)
-				}
+				unitHandler(unit)
+			case order := <- orderChan:
+				//add order to stack
 			}
+			//sync
+			//handleorders
 
 		case false:
 			select {
 			case update := <- masterSync:
 				masterOrderList = update
 			default:
-				checkIfActive()
+				
 			}
 		}
+		checkIfActive()
 		/*
 			- Hvem er på nettverket?
 			- Lag lister over heiser og mastere
