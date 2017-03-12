6e28adfea0d80d1a6e2748195cb65746f8114eaf
Sveinung Haugane <sveihaug@gmail.com>
2017-03-12 16:58:08 +0100

    Lagd order handling i testmaster

--- src/master/testmaster/testmaster.go
+++ src/master/testmaster/testmaster.go
@@ -11,14 +11,17 @@ func main() {
 	fmt.Println("Running tests!")
 
 	reportNum := 0
-	unitChan := make(chan UnitUpdate)
+	unitChan := make(chan UnitUpdate, 1)
+
+	orderTx := make(chan OrderType, 10)
+	orderRx := make(chan OrderType, 10)
 
 	statusReqChan := make(chan int, 1)
 	statusChan := make(chan StatusType, 10)
 	quit := make(chan bool)
-	go testUnits(unitChan, quit)
+	go testint(unitChan, orderTx, orderRx, quit)
 
-	testMNI.Init_tmni(statusReqChan, statusChan, unitChan, quit)
+	testMNI.Init_tmni(statusReqChan, statusChan, unitChan, orderTx, orderRx, quit)
 
 	for {
 		time.Sleep(1 * time.Second)
@@ -36,13 +39,22 @@ func main() {
 	}
 }
 
-func testUnits(unitChan <-chan UnitUpdate, quit <-chan bool)
+func testing(unitChan <-chan UnitUpdate, orderTx chan<- OrderType, orderRx <-chan OrderType, quit <-chan bool)
 {
 	var units UnitUpdate
+	orderTime := time.Tick(3000 * time.Millisecond)
+	orders := make(chan OrderType, 15)
+
+
+
 	for {
 		select {
 		case units = <- unitChan:
 			fmt.Println("Got units:", units)
+		case order := <- orderRx:
+			fmt.Println("Got order:", order)
+		case <- orderTime:
+			orderTx <- OrderType{To: "Jarvis", From: "Carl", Floor: 2, Dir: 0, New: true}
 		case <- quit:
 			fmt.Println("quitting units")
 			return
